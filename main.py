73f7fb94e5e52c4839e80ffa693f637a82811089
mhhuynh206 <huynh_mychael@yahoo.com>
2020-12-10 16:48:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
+# Mychael Huynh
 # CNA 337 Fall 2020
+# Received help from Hasan
 
+from Server import Server
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
 
-# This is the entry point to our program
+    print("Server Automator v0.1 by Mychael")
+
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+    EC2server = Server('18.221.253.11')
+
+    print(EC2server.ping())
